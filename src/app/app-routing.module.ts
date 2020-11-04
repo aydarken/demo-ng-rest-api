e3c56eb2af9b6b21c7e419115516aa15e15fe182
Nurbek Aidarken <nurbek0144@gmail.com>
2020-11-04 09:03:28 +0600

    add routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {PageComponent} from './home/page.component';
+import {AboutComponent} from './about/about.component';
+import {ProfileComponent} from './profile/profile.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'home', component: PageComponent },
+  {path: 'about', component: AboutComponent},
+  {path: 'profile', component: ProfileComponent},
+  {path: '', redirectTo: 'home', pathMatch: 'full'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
